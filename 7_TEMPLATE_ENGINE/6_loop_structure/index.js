273e934584fe2d0e34d82f1100cc831b0a6174d5
Luiz <lastefani@cliptech.com.br>
2022-01-11 14:29:48 -0300

    Finalizado Template Engine com Express

---
+++ 7_TEMPLATE_ENGINE/6_loop_structure/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/dashboard", (req, res) => {
+  const itens = ["Item a", "Item b", "Item c"];
+
+  res.render("dashboard", { itens });
+});
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Luiz",
+    surname: "Stefani",
+    age: 21,
+  };
+
+  const palavra = "Teste";
+
+  const auth = true;
+
+  const approved = true;
+
+  res.render("home", { user: user, palavra, auth, approved });
+});
+
+app.listen(3000, () => {
+  console.log("App funcionando");
+});
