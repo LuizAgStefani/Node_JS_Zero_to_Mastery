69fc848d63a11d2763b7677cc8dc297f34b0bdd5
Luiz <lastefani@cliptech.com.br>
2022-01-13 14:36:30 -0300

    Finalizado Sequelize

---
+++ 9_SEQUELIZE/4_reading_data/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("nodesequelize", "root", "biagio04", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+// try {
+//   sequelize.authenticate();
+//   console.log("Conectamos com sucesso com o Sequelize");
+// } catch (error) {
+//   console.log(`Não foi possível conectar: ${error}`);
+// }
+
+module.exports = sequelize;
